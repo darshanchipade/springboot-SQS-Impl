43b944f42fcf5524fc6ad46d1d3920bdb6bf152f
darshanchipade <dchipade@gmail.com>
2025-12-01 12:19:27 -0800

    updated the rate limit, embedQPS for faster processing of the embeddings

--- src/main/java/com/apple/springboot/config/ChunkingConfig.java
+++ src/main/java/com/apple/springboot/config/ChunkingConfig.java
@@ -2,6 +2,6 @@ package com.apple.springboot.config;
 
 public class ChunkingConfig {
     public static final int LENGTH_THRESHOLD = 500; // chars
-    public static final int SENTENCES_PER_CHUNK = 2;
+    public static final int SENTENCES_PER_CHUNK = 6;
     public static final int SENTENCE_OVERLAP = 1;
 }
