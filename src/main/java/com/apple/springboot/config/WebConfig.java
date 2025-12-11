6985eede5921dc49696758e3f5ebfb657c1b10a1
darshanchipade <dchipade@gmail.com>
2025-12-10 23:16:45 -0800

    Create WebConfig.java

---
+++ src/main/java/com/apple/springboot/config/WebConfig.java
@@ -0,0 +1,23 @@
+package com.apple.springboot.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig {
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("https://user-flow-coral.vercel.app")
+                        .allowedMethods("*")
+                        .allowedHeaders("*");
+            }
+        };
+    }
+}
